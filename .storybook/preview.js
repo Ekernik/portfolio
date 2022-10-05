4b4ae2f50741617a2318af7f08f8d3558fd7e79b
Ekernik <ekernik@gmail.com>
2022-10-05 23:09:02 +0300

    test: add breakpoints to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,30 @@
 import * as NextImage from 'next/future/image';
 import '../styles/main.scss';
 
+const BREAKPOINTS_INT = {
+  xs: 375,
+  sm: 600,
+  md: 900,
+  lg: 1200,
+  xl: 1536,
+};
+
+const customViewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    console.log(val);
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ];
+  })
+);
+
 const OriginalNextImage = NextImage.default;
 
 Object.defineProperty(NextImage, 'default', {
@@ -8,4 +32,7 @@ Object.defineProperty(NextImage, 'default', {
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
 
-export const parameters = { layout: 'fullscreen' };
+export const parameters = {
+  layout: 'fullscreen',
+  viewport: { viewports: customViewports },
+};
