a021d4ab3ab854e91db66e350b920f02678b4fc7
Ekernik <ekernik@gmail.com>
2022-10-08 18:37:20 +0300

    feat: implement basic seo

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,21 @@ import { Head, Html, Main, NextScript } from 'next/document';
 function Document() {
   {
     return (
-      <Html lang="en_US" className="scroll-smooth">
+      <Html lang="en-US" className="scroll-smooth">
         <Head>
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" defer />
+          <title>Nikita Ekern - Web Developer</title>
+          <meta property="og:title" content="Nikita Ekern - Web Developer" />
+          <meta
+            name="description"
+            content="Nikita Ekern's personal website - developing modern and reliable websites using latest technologies"
+          />
+          <meta
+            property="og:description"
+            content="Nikita Ekern's personal website - developing modern and reliable websites using latest technologies"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://ekernik.com" />
         </Head>
         <body>
           <Main />
