6fe432c180e48730c4fa889830e624a3b6803e05
Ekernik <ekernik@gmail.com>
2022-10-08 18:39:52 +0300

    fix: move title and description from _document to index page

--- pages/_document.js
+++ pages/_document.js
@@ -6,16 +6,6 @@ function Document() {
       <Html lang="en-US" className="scroll-smooth">
         <Head>
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" defer />
-          <title>Nikita Ekern - Web Developer</title>
-          <meta property="og:title" content="Nikita Ekern - Web Developer" />
-          <meta
-            name="description"
-            content="Nikita Ekern's personal website - developing modern and reliable websites using latest technologies"
-          />
-          <meta
-            property="og:description"
-            content="Nikita Ekern's personal website - developing modern and reliable websites using latest technologies"
-          />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://ekernik.com" />
         </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,32 @@ import Introduction from '@/components/sections/Introduction';
 import MyWork from '@/components/sections/MyWork';
 import TechStack from '@/components/sections/TechStack';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => (
-  <div className="flex max-h-max min-h-screen flex-col gap-20 bg-slate-50">
-    <Header />
-    <Introduction />
-    <AboutMe />
-    <TechStack />
-    <MyWork />
-    <Contacts />
-    <Footer />
-  </div>
+  <>
+    <Head>
+      <title>Nikita Ekern - Web Developer</title>
+      <meta property="og:title" content="Nikita Ekern - Web Developer" />
+      <meta
+        name="description"
+        content="Nikita Ekern's personal website - developing modern and reliable websites using latest technologies"
+      />
+      <meta
+        property="og:description"
+        content="Nikita Ekern's personal website - developing modern and reliable websites using latest technologies"
+      />
+    </Head>
+    <div className="flex max-h-max min-h-screen flex-col gap-20 bg-slate-50">
+      <Header />
+      <Introduction />
+      <AboutMe />
+      <TechStack />
+      <MyWork />
+      <Contacts />
+      <Footer />
+    </div>
+  </>
 );
 
 export default Home;
