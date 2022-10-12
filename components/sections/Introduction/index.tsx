f240464f68237c826799b9c650a01bfca459ef33
Ekernik <ekernik@gmail.com>
2022-10-12 17:55:20 +0300

    feat: add CV and buttons to download it, change some text

--- components/sections/Introduction/index.tsx
+++ components/sections/Introduction/index.tsx
@@ -1,4 +1,5 @@
 import SocialLinks from '@/components/SocialLinks';
+import { ArrowDownTrayIcon } from '@heroicons/react/24/outline';
 import scrollToSection from 'helpers/scrollToSection';
 import React from 'react';
 
@@ -7,18 +8,19 @@ const Introduction: React.FC = () => (
     <p className="mt-20 text-sm text-slate-600 dark:text-slate-400 lg:text-base">
       Hi, my name is
     </p>
-    <h1 className="-ml-1 text-4xl font-bold text-slate-800 dark:text-slate-200 lg:text-6xl">
+    <h1 className="text-4xl font-bold text-slate-800 dark:text-slate-200 sm:-ml-1 lg:text-6xl">
       Nikita Ekern
     </h1>
     <p className="mb-8 border-b text-xl font-bold text-slate-700 dark:border-b-slate-500 dark:text-slate-300 lg:text-2xl">
-      I am a Frontend developer
+      Frontend developer
     </p>
     <p className="paragraph mb-2">
-      I specialize in building fast loading <span>websites</span> with proper
-      semantics, acessebility in mind and SEO-friendliness.
+      I specialize on building accessible and fast web application to solve my
+      clients problems.
     </p>
     <p className="paragraph mb-10">
-      I&apos;m looking forward working with you!
+      I value simple content structure, clean design patterns and love bringing
+      meaningful ideas to life in the browser.
     </p>
     <SocialLinks />
     <div className="flex w-full justify-center gap-4 sm:justify-start">
@@ -28,13 +30,22 @@ const Introduction: React.FC = () => (
       >
         See my work!
       </button>
-      <a
-        // TODO: add resume
-        href="#"
-        className="button border-slate-800 text-slate-800 hover:bg-slate-800 hover:text-slate-50 dark:bg-slate-800 dark:text-slate-400 dark:hover:border-slate-700 dark:hover:bg-slate-700 dark:focus:border-slate-700 dark:focus:bg-slate-700 dark:focus:text-slate-50"
-      >
-        Download resume
-      </a>
+      <div className="flex">
+        <a
+          href="/CV%20-%20Nikita%20Ekern%20(Frontend%20developer).pdf"
+          target="_tab"
+          className="button button--secondary rounded-r-none border-r-0"
+        >
+          Resume
+        </a>
+        <a
+          href="/CV%20-%20Nikita%20Ekern%20(Frontend%20developer).pdf"
+          className="button button--secondary rounded-l-none  px-4 dark:border-l dark:border-l-slate-600"
+          download
+        >
+          <ArrowDownTrayIcon className="h-6 w-6" />
+        </a>
+      </div>
     </div>
   </section>
 );
