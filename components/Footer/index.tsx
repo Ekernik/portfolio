63a18e700ec3581221f81bd270f081a00e913eb4
Ekernik <ekernik@gmail.com>
2022-10-31 13:59:27 +0200

    refactor: change text

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const Footer: FC = () => {
   return (
     <footer className="mt-4 flex h-12 items-center justify-center bg-slate-50 text-xs font-bold dark:bg-gray-900 dark:text-slate-400">
-      &copy; 2022 Ekern Nikita - Web Developer
+      &copy; 2022 Nikita Ekern - Web Developer
     </footer>
   );
 };
