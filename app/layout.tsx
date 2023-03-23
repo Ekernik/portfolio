f4492a58c3d3fc0cd47f91db55f62801ef89543d
Ekernik <ekernik@gmail.com>
2023-03-23 04:12:44 +0400

    refactor: seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,31 +21,41 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => {
             document.documentElement.classList.remove('dark');
           }`}
         </Script>
-        <title>Nikita Ekern - Web Developer</title>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="Nikita Ekern - Web Developer" />
+        <link rel="shortcut icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
+        <title>Nikita Ekern - Web Developer</title>
         <meta
           name="description"
-          content="Nikita Ekern's personal website - developing modern and reliable websites using latest technologies"
+          content="Developing modern and reliable websites using latest technologies"
         />
+
+        <meta property="og:url" content="https://www.ekernik.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Nikita Ekern - Web Developer" />
         <meta
           property="og:description"
-          content="Nikita Ekern's personal website - developing modern and reliable websites using latest technologies"
+          content="Developing modern and reliable websites using latest technologies"
         />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.ekernik.com/" />
         <meta
           property="og:image"
           content="https://www.ekernik.com/website.png"
         />
+
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ekernik.com" />
+        <meta property="twitter:url" content="https://ekernik.com" />
+        <meta name="twitter:title" content="Nikita Ekern - Web Developer" />
+        <meta
+          name="twitter:description"
+          content="Developing modern and reliable websites using latest technologies"
+        />
         <meta
           name="twitter:image"
           content="https://www.ekernik.com/website.png"
         />
-        <link rel="shortcut icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body>
         <Header />
